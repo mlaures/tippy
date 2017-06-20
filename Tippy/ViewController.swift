9409b28870697a05ffd38f08095c0202496510d8
Mei-Ling Laures <mlaures@fb.com>
2017-06-20 15:29:35 -0700

    Added code to deal with tapping interactions and calculating the tip in real time

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bill_entry: UITextField!
+    @IBOutlet weak var total_label: UILabel!
+    @IBOutlet weak var tip_label: UILabel!
+    @IBOutlet weak var tip_control: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculate_tip(_ sender: Any) {
+        
+        let percentages = [0.15, 0.18, 0.2];
+        let amount = Double(bill_entry.text!) ?? 0;
+        
+        let calc = amount * percentages[tip_control.selectedSegmentIndex];
+        
+        
+        tip_label.text = String(format: "$%.2f", calc);
+        total_label.text = String(format: "$%.2f", (calc+amount));
+    }
 
+    @IBAction func on_tap(_ sender: Any) {
+        view.endEditing(true);
+    }
 }
 
