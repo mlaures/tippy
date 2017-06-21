df297dcb8516f155ebe528e7b613f1de48fa3ca8
Mei-Ling Laures <mlaures@fb.com>
2017-06-21 16:50:00 -0700

    added comments to code

--- Tippy/SettingsViewController.swift
+++ Tippy/SettingsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    // Outlets that must be referenced from the settings screen
     @IBOutlet weak var seg1: UITextField!
     @IBOutlet weak var seg2: UITextField!
     @IBOutlet weak var seg3: UITextField!
@@ -25,41 +26,55 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /* function: To change the preferred tip percentages
+     * param: N/A
+     * returns: N/A
+     * This function will take whatever the user inputs in the three options for percentages and store the value within the global variables for later
+     */
     @IBAction func segment_change(_ sender: UITextField) {
+        // check if the inputs are valid numbers
         let per1 = Double(seg1.text!) ?? 0;
         let per2 = Double(seg2.text!) ?? 0;
         let per3 = Double(seg3.text!) ?? 0;
         
+        // set the global variables to be accessed later
         let defaults = UserDefaults.standard;
         defaults.set(per1, forKey: "seg_op1");
         defaults.set(per2, forKey: "seg_op2");
         defaults.set(per3, forKey: "seg_op3");
         defaults.synchronize();
         
-        seg1.text = String(defaults.integer(forKey: "seg_op1"));
-        seg2.text = String(defaults.integer(forKey: "seg_op2"));
-        seg3.text = String(defaults.integer(forKey: "seg_op3"));
+        // reset the text of the segments to properly display what has been stored
+        seg1.text = defaults.string(forKey: "seg_op1");
+        seg2.text = defaults.string(forKey: "seg_op2");
+        seg3.text = defaults.string(forKey: "seg_op3");
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Make sure the displayed changeable tip percentages are what are actually stored in the global variables
         let defaults = UserDefaults.standard;
-        
-        seg1.text = String(defaults.integer(forKey: "seg_op1"));
-        seg2.text = String(defaults.integer(forKey: "seg_op2"));
-        seg3.text = String(defaults.integer(forKey: "seg_op3"));
+        seg1.text = defaults.string(forKey: "seg_op1");
+        seg2.text = defaults.string(forKey: "seg_op2");
+        seg3.text = defaults.string(forKey: "seg_op3");
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
 
 }
--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /* Outlets which must be changed through the functions */
     @IBOutlet weak var bill_entry: UITextField!
     @IBOutlet weak var total_label: UILabel!
     @IBOutlet weak var tip_label: UILabel!
@@ -25,32 +26,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /* function: To calculate the tip
+     * param: N/A
+     * returns: N/A
+     * This function directly uses connected outlets in order to change the text values that they have.
+     */
     @IBAction func calculate_tip(_ sender: Any) {
         
         let defaults = UserDefaults.standard;
         
+        // make an array of percentages which user has set as default
         let percentages = [defaults.double(forKey: "seg_op1"), defaults.double(forKey: "seg_op2"), defaults.double(forKey: "seg_op3")];
+        // store the input that is in the text entry field
         let amount = Double(bill_entry.text!) ?? 0;
         
+        // do the calculation for the tip
         let calc = amount * percentages[tip_control.selectedSegmentIndex];
         
+        // update the displays for the tip and the total
         tip_label.text = String(format: "$%.2f", calc);
         total_label.text = String(format: "$%.2f", (calc+amount));
+        
+        
+        
     }
 
+    // function: To put away the keyboard
     @IBAction func on_tap(_ sender: Any) {
         view.endEditing(true);
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // recalculate the tip whenever this screen appears
         self.calculate_tip(self);
         
+        // make sure the tip options are up-to-date from the global variables
         let defaults = UserDefaults.standard;
-        tip_control.setTitle(String(defaults.integer(forKey: "seg_op1")) + "%", forSegmentAt: 0);
-        tip_control.setTitle(String(defaults.integer(forKey: "seg_op2")) + "%", forSegmentAt: 1);
-        tip_control.setTitle(String(defaults.integer(forKey: "seg_op3")) + "%", forSegmentAt: 2);
+        tip_control.setTitle(defaults.string(forKey: "seg_op1")! + "%", forSegmentAt: 0);
+        tip_control.setTitle(defaults.string(forKey: "seg_op2")! + "%", forSegmentAt: 1);
+        tip_control.setTitle(defaults.string(forKey: "seg_op3")! + "%", forSegmentAt: 2);
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
     }
 
 }
